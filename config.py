c0b2ba743ebac33acf311afc2818354a0d098c0f
Vaibhav Gawande <108888490+vaibhavgawande-5050@users.noreply.github.com>
2025-03-24 08:07:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-test_key='rzp_test_LFnpHYpUR9cUXG'  #add your razorpay key here
-test_secret='pLjtSxNrz4hZqu1IpdXPqLdy' #add your razorpay secret here
+test_key='add test key here'  #add your razorpay key here
+test_secret='add test secret  here' #add your razorpay secret here
 
 #above is test key and test secret i will gonna delete it after code is uploaded
 #use test ksy for development use razorpay in test mode
